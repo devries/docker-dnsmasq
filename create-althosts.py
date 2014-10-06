d1a5e6da639f88a44127504ebbdf968cf908e0c3
Christopher De Vries <devries@idolstarastronomer.com>
2014-10-06 15:11:53 -0700

    Added a script to create a hosts file from running containers

---
+++ create-althosts.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import subprocess
+import json
+
+p1 = subprocess.Popen(['docker','ps','-q'],stdout=subprocess.PIPE)
+
+process_list = [p for p in p1.communicate()[0].split('\n') if p]
+
+p2 = subprocess.Popen(['docker','inspect']+process_list,stdout=subprocess.PIPE)
+
+inspect_data = json.loads(p2.communicate()[0])
+
+for container in inspect_data:
+    print container['NetworkSettings']['IPAddress'],connection['Name'].lstrip('/')
